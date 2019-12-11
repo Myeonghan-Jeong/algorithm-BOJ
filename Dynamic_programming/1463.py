dd24a94d116085ddae22f4d4b8aa46b32c2638ff
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-12-11 21:18:15 +0900

    modify DP examples

--- Dynamic_programming/1463.py
+++ Dynamic_programming/1463.py
@@ -1,4 +1,4 @@
-# 1463 / calculate min of using the lowest calculation
+# 1463 / calculate min of using calculation
 info = {1: 0, 2: 1}
 
 
@@ -7,9 +7,7 @@ def find(n):  # set function with DP
     if n in info:  # check number is in dict
         return info[n]
 
-    # find min number of calculation
-    # n // k: number of divison calculation
-    # n % k: number of minus calculation
+    # //, %: number of divison, number of minus
     m = 1 + min(find(n // 2) + n % 2, find(n // 3) + n % 3)
     info[n] = m  # add result to dict
 
