a16842fccd4d5925eb99f87441decf1b389a6adb
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-12-16 21:21:00 +0900

    add print examples

---
+++ Print/10171; 10172;.py
@@ -0,0 +1,12 @@
+# 10171 / print cat
+print('\    /\\')
+print(' )  ( \')')
+print('(  /  )')
+print(' \(__)|')
+
+# 10172 / print dog
+print('|\_/|')
+print('|q p|   /}')
+print('( 0 )\"\"\"\\')
+print('|\"^\"`    |')
+print('||_/=\\\\__|')
