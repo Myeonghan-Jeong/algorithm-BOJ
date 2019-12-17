ce09dab6a76f5155d25f2503aeb9bc2764ffa824
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-12-18 00:35:26 +0900

    add DP examples

---
+++ Dynamic_programming/17070.py
@@ -0,0 +1,23 @@
+# 17070 / count number of routes can reach final point
+N = int(input())
+room = [list(map(int, input().split())) for _ in range(N)]
+
+# set DP matrix with has to row, to col, to diag in each points
+dp = [[[0] * 3 for _ in range(16)] for _ in range(16)]
+dp[0][1][0] = 1
+
+# set first row
+for i in range(2, N):
+    if not room[0][i]:
+        dp[0][i][0] = dp[0][i - 1][0]
+
+# move if there is no block
+for i in range(1, N):
+    for j in range(2, N):
+        if not room[i][j] and not room[i][j - 1] and not room[i - 1][j]:
+            dp[i][j][1] = sum(dp[i - 1][j - 1])
+        if not room[i][j]:
+            dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][1]
+            dp[i][j][2] = dp[i - 1][j][1] + dp[i - 1][j][2]
+
+print(sum(dp[N - 1][N - 1]))
