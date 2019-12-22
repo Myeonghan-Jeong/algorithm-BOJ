542f26b697d4bb0163936a3ec9334cf562e12c97
Myeonghan-Jeong <wjdaudgks94@naver.com>
2019-12-22 20:01:08 +0900

    rename folder

---
+++ dp/1463.py
@@ -0,0 +1,18 @@
+# 1463 / calculate min of using calculation
+info = {1: 0, 2: 1}
+
+
+def find(n):  # set function with DP
+
+    if n in info:  # check number is in dict
+        return info[n]
+
+    # //, %: number of divison, number of minus
+    m = 1 + min(find(n // 2) + n % 2, find(n // 3) + n % 3)
+    info[n] = m  # add result to dict
+
+    return m
+
+
+N = int(input())
+print(find(N))
