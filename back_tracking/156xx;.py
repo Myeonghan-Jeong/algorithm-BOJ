e03529a05ba1edaddd49d2c12453d0da469b1faa
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-02-27 11:03:58 +0900

    add back tracking examples

---
+++ back_tracking/156xx;.py
@@ -0,0 +1,64 @@
+# 15649 / make permutes with select M numbers in N numbers
+
+
+def npr(n, k, N):
+    global permute, used
+
+    if n == k:
+        permuted = ' '.join(list(map(str, permute)))
+        print(permuted)
+    else:
+        for i in range(N):
+            if not used[i]:
+                used[i] = 1
+                permute[n] = i + 1
+                npr(n + 1, k, N)
+                used[i] = 0
+
+
+N, M = map(int, input().split())
+
+permute = [0] * M
+used = [0] * N
+npr(0, M, N)
+
+# 15650 / make permutes with ascending and select M numbers in N numbers
+
+
+def npr(n, s, k, N):
+    global permute
+
+    if n == k:
+        permuted = ' '.join(list(map(str, permute)))
+        print(permuted)
+    else:
+        for i in range(s, N):
+            permute[n] = i + 1
+            npr(n + 1, i + 1, k, N)
+
+
+N, M = map(int, input().split())
+
+permute = [0] * M
+npr(0, 0, M, N)
+
+# 15651 / make permutes with duplicated and select M numbers in N numbers
+
+
+def npr(n, r, t):
+    global permute
+
+    if n == r:
+        for p in permute:
+            print(p, end=' ')
+        print()
+    else:
+        for i in range(t):
+            permute[n] = i + 1
+            npr(n + 1, r, t)
+
+
+N, M = map(int, input().split())
+
+permute = [0] * M
+npr(0, M, N)
