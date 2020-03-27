c9143fda4360dfc9336509ce3ad81c7ff9806062
Myeonghan-Jeong <ataraxia9408@gmail.com>
2020-03-27 11:11:37 +0900

    modify back tracking examples

--- back_tracking/14889.py
+++ back_tracking/14889.py
@@ -1,28 +1,28 @@
 # 14889 / calculate min diff of team start and team link
-from collections import deque
+from itertools import combinations
 
 N = int(input())
 people = [list(map(int, input().split())) for _ in range(N)]
+teams = list(combinations(range(N), N // 2))  # use combination
 
-res = float('inf')
-for i in range(1 << N):
-    start, link = deque(), deque()  # devide teams
-    for j in range(N):
-        if i & (1 << j):
-            start.append(j)
-        else:
-            link.append(j)
+res = float('INF')
+for team in teams:
+    start, link = team, tuple(set(range(N)) - set(team))
 
-    if len(start) > 1 and len(link) > 1:  # calculate sum of score
-        s = 0
-        for i in range(len(start)):
-            for j in range(i + 1, len(start)):
-                s += people[start[i]][start[j]] + people[start[j]][start[i]]
-        l = 0
-        for i in range(len(link)):
-            for j in range(i + 1, len(link)):
-                l += people[link[i]][link[j]] + people[link[j]][link[i]]
-        diff = abs(s - l)
-        if res > diff:
-            res = diff
+    r1 = 0  # calculate sum of each team
+    for p1 in start:
+        for p2 in start:
+            if p1 != p2:
+                r1 += people[p1][p2]
+
+    r2 = 0
+    for q1 in link:
+        for q2 in link:
+            if q1 != q2:
+                r2 += people[q1][q2]
+
+    diff = abs(r1 - r2)  # check diff
+    if res > diff:
+        res = diff
+
 print(res)
