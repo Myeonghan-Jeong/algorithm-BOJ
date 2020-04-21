9b8e0353afcc645279d264a3c35a859bb875f6c2
Myeonghan-Jeong <wjdaudgks94@naver.com>
2020-04-21 20:30:33 +0900

    add stack examples

---
+++ stack/10773.py
@@ -0,0 +1,11 @@
+# 10773 / calculate sum of stack after program
+import sys
+
+s = []
+for _ in range(int(input())):
+    n = int(sys.stdin.readline())
+    if n == 0:
+        s.pop()
+    else:
+        s.append(n)
+print(sum(s))
